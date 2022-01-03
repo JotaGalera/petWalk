e91848c82041478d1900754f4fccec60be4e88da
Javier Galera <javier.galera@bestsecret.com>
2022-01-03 19:58:45 +0100

    Closed #1 first pet screen

--- petWalk/petWalk WatchKit Extension/ContentView.swift
+++ petWalk/petWalk WatchKit Extension/ContentView.swift
@@ -9,8 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Spacer()
+            PetScene(petImageName: "pet",
+                     petName: "Beldrick")
+            Spacer()
+            StatusBarMenu()
+        }
+    }
+}
+
+struct PetScene: View {
+    let width: CGFloat = 80
+    let height: CGFloat = 80
+    var petImageName: String
+    var petName: String
+    
+    var body: some View {
+        VStack {
+            Text(petName)
+                .bold()
+            Spacer().frame(height: 15)
+            Image(petImageName)
+                .resizable()
+                .frame(width: width,
+                       height: height,
+                       alignment: .center)
+        }
+    }
+}
+
+struct StatusBarMenu: View {
+    var body: some View {
+        HStack {
+            ItemBarMenu(imageName: "Sword",
+                        valueText: "10")
+            ItemBarMenu(imageName: "Heart",
+                        valueText: "10")
+        }
+    }
+}
+
+struct ItemBarMenu: View {
+    let width: CGFloat = 25
+    let height: CGFloat = 25
+    var imageName: String
+    var valueText: String
+    
+    var body: some View {
+        HStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 25,
+                       height: 25,
+                       alignment: .center)
+            Text("10")
+        }
+        .padding()
     }
 }
 
