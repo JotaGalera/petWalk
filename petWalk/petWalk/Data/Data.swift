bdeff3cd9ce1c007570ee72489ccb40b16b4e578
Javier Galera <javier.galera@bestsecret.com>
2022-11-03 21:24:23 +0100

    Closed #44 Create the structure of the project: Data - Domain - Presentation

---
+++ petWalk/petWalk/Data/Data.swift
@@ -0,0 +1,12 @@
+//
+//  Data.swift
+//  petWalk
+//
+//  Created by Galera, Javier on 3/11/22.
+//
+
+import Foundation
+
+protocol DataSource: AutoMockable {
+    func x()
+}
---
+++ petWalk/petWalk/Domain/useCase.swift
@@ -0,0 +1,12 @@
+//
+//  useCase.swift
+//  petWalk
+//
+//  Created by Galera, Javier on 3/11/22.
+//
+
+import Foundation
+
+protocol useCase: AutoMockable {
+    func x()
+}
