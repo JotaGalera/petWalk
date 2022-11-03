8e504f9f33d3ce389c36f36a265a160fb8490134
Javier Galera <javier.galera@bestsecret.com>
2022-11-03 21:24:46 +0100

    Closed #45 Include Sourcery in iOS App

--- petWalk/petWalkTests/petWalkTests.swift
+++ petWalk/petWalkTests/petWalkTests.swift
@@ -9,7 +9,6 @@ import XCTest
 @testable import petWalk
 
 class petWalkTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
